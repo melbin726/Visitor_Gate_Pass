6da63cc8c45bfd01487e209bc4952d893f561d0d
AnthonyPint0 <antoniocruze17@gmail.com>
2024-07-05 15:22:10 +0530

    KJC_Logo_Blue.svg Added!!!

--- KJC_Gate_Pass/src/TextImage.jsx
+++ KJC_Gate_Pass/src/TextImage.jsx
@@ -2,7 +2,6 @@
 
 function TextImage(){
     return(
-<<<<<<< HEAD
     <div className="textImage">
         <div className="textOnly">
             <p id="text1">Security Management</p>
@@ -11,16 +10,6 @@ function TextImage(){
         <img src="./src/assets/Image_LoginPage.svg" alt="LogoPIC.png" />
     </div>
 );
-=======
-        <div className="textImage">
-            <div className="texts">
-                <p id="text1">Security Management</p>
-                <p id="text2">Rendering our service for the others</p>
-            </div>            
-            <img src="./src/assets/Image_LoginPage.svg" alt="Image_LoginPage.svg" />
-        </div>        
-    );
->>>>>>> 1ca122ba26fb4fc5efff21e4596bfe193db0583a
 }
 
 export default TextImage;

01fe0870e8d5f6dc6e899e78ecc22fa4a8bd5aea
Mohammed Yunus <mohammedyunus4t6@gmail.com>
2024-07-07 01:54:42 +0530

    ID Card Selector Component

--- KJC_Gate_Pass/src/App.jsx
+++ KJC_Gate_Pass/src/App.jsx
@@ -1,10 +1,16 @@
-function App() {
+// App.js
+import React from 'react';  
+import Taskbar from './components/Taskbar/taskbar';
+import IDCardBooking from './components/IDCardBooking/IDCardBooking';
+import './App.css'; // Import the CSS file for additional styles
 
-  return (
-    <>
-      
+const App = () => {
+  
+   return (
+    <> 
+      <IDCardBooking/>
     </>
-  );
-}
+  )
+};
 
 export default App

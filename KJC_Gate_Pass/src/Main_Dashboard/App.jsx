3327b3f61f817bbfd6dffd8863c190ed4169ffe2
DarkPrinceLD <antoniocruze17@gmail.com>
2024-07-06 15:14:14 +0530

    Added Widgets!!!

--- KJC_Gate_Pass/src/Main_Dashboard/App.jsx
+++ KJC_Gate_Pass/src/Main_Dashboard/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import SideBarNavi from '../My_Components/SideBarNavi/SideBarNavi.jsx';
+import DashboardWidget from './DashboardWidget.jsx'
 
 function App() {
   return (
@@ -7,6 +8,17 @@ function App() {
     <div className="fakeBody">
       <div className="totalContent">
         <SideBarNavi />
+        <div className="content">
+          <div className="fakeSideBAr" />
+          <div className="Widgets">
+            <DashboardWidget isCountWidget={true} widgets='totalVisitorCount' title='Total Visitor' count={46}/>
+            <DashboardWidget isCountWidget={true} widgets='checkinVisitorCount' title='Check-in Visitor' count={14}/>
+            <DashboardWidget isCountWidget={true} widgets='checkoutVisitorCount' title='Check-out Visitor' count={32}/>
+            <DashboardWidget isCountWidget={false} widgets='registerVisitor' title='Register Visitor'/>
+            <DashboardWidget isCountWidget={false} widgets='checkinVisitor' title='Check-in Visitor'/>
+            <DashboardWidget isCountWidget={false} widgets='checkoutVisitor' title='Check-out Visitor'/>
+          </div>
+        </div>
       </div>
     </div>
   );

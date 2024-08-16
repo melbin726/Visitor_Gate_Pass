8d69176ff9d6eb6f17eb4682f4397988d71e09e9
Isaac Thapa <isaacthapa36@gmail.com>
2024-08-16 12:50:41 +0530

    all the visitor_files added

--- KJC_Gate_Pass/src/App.jsx
+++ KJC_Gate_Pass/src/App.jsx
@@ -4,6 +4,8 @@ import LoginPage from './pages/LoginPage_KJC/LoginPage.jsx';
 import Dashboard from './pages/Main_Dashboard/Dashboard.jsx';
 import Register_Visitor from './pages/Register_Visitor/Register_Visitor.jsx';
 import Checkout_Visitor from './pages/Checkout_Visitor/Checkout_Visitor.jsx';
+import Visitor_Details from './pages/Visitor_Details/Visitor_Details.jsx';
+
 
 function App() {
     return (
@@ -13,6 +15,8 @@ function App() {
                 <Route path='/dashboard' element={<Dashboard />} />
                 <Route path='/register_visitor' element={<Register_Visitor />} />
                 <Route path='/checkout_visitor' element={<Checkout_Visitor />} />
+                <Route path='/Visitor_Details' element={<Visitor_Details />} />
+
             </Routes>
         </BrowserRouter>
     );
